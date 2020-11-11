f3a0c41f0478ce167b0960140e050a58fbc7e2ed
ichrystm <ichrystm@gmail.com>
2020-11-10 22:39:34 -0300

    Insert new vehicle

--- newentry/NewentryController.js
+++ newentry/NewentryController.js
@@ -8,6 +8,19 @@ router.get("/newentry", (req, res) => {
 
 router.post("/newentry/new", (req, res) => {
   var plate = req.body.plate;
+  var manufacturer = req.body.manufacturer;
+  var model = req.body.model;
+
+  Newentry.create({
+    plate: plate,
+    manufacturer: manufacturer,
+    model: model,
+  }).then(() => {
+    res.redirect("/");
+  }).catch((err) => {
+    console.log(err);
+  })
+
 })
 
 module.exports = router;

bdab6df781c2998d8ff40dfff5bcdf4d173b3cca
ichrystm <ichrystm@gmail.com>
2020-11-15 19:53:59 -0300

    Update Historic view schema

--- historic/HistoricController.js
+++ historic/HistoricController.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const entry = require("../newentry/Newentry");
 
 router.get("/historic", (req, res) => {
-  res.render("historic");
+  entry.findAll({ raw: true, order:[['entrydate', 'DESC']]})
+  .then(entrys => {
+    res.render('historic', {
+      entrys: entrys
+    })
+  })
 });
 
 module.exports = router;
